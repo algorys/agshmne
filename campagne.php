15fb6fb5bcf76482477f2cf076e3c3667af61540
Estrada Matthieu <ttamalfor@gmail.com>
2015-11-30 00:34:33 +0100

    test bouton

---
+++ campagne.php
@@ -0,0 +1,9 @@
+<?php
+
+echo '<form name="Deplace" action="map.php" method="POST">';
+echo '<input type="submit" name="dir" value="haut" />';
+echo '<input type="submit" name="dir" value="gauche" />';
+echo '<input type="submit" name="dir" value="droite" />';
+echo '<input type="submit" name="dir" value="bas" />';
+echo '</form>';
+
