6f59c17acafc5ec2c9490874fc100c8ba3f1ac35
Matthieu Estrada <m.estrada@alpi.fr>
2015-11-30 16:29:41 +0100

    Refonte des objets

--- move.php
+++ move.php
@@ -1,68 +1,99 @@
 <?php
 
-class Deplacer {
+class Emplacement {
+    private $x;
+    private $y;
 
-    /***************
-    * Coordonnées initiales du PJ
-    */
-    var $x;
-    var $y;
-
-    /*
-    * Quatres déplacements possible (est, ouest, sud, nord)
-    */
-    function monter() {
-
-        $this->y += 1;
+    function Emplacement($x, $y) {
+        $this->x = $x;
+        $this->y = $y;
     }
 
-    function descendre() {
+    function getX() { return $this->x; }
+    function getY() { return $this->y; }
 
-        $this->y -= 1;
+    function getVoisin($direction) {
+        $x = $this->x;
+        $y = $this->y;
+
+        switch($direction) {
+            case Direction::NORTH:
+                $y = $this->y + 1;
+            break;
+            case Direction::EAST:
+                $x = $this->x + 1;
+            break;
+            case Direction::SOUTH:
+                $y = $this->y - 1;
+            break;
+            case Direction::WEST:
+                $x = $this->x - 1;
+            break;
+            default:
+                //ERROR
+        }
+        return new Emplacement($x, $y);
     }
-
-    function gauche() {
-
-        $this->x -= 1;
-    }
-
-    function droite() {
-
-        $this->x += 1;
-    }
-
-    function afficherPosition() {
-
-        echo "Vous vous trouvez en : <br>";
-
-        echo "x = : ".$this->x;
-
-        echo " y = : ".$this->y;
-
-        echo '<br>';
-    }
-
 }
 
-$pj = new Deplacer();
+abstract class Direction {
+    const NORTH = 0;
+    const EAST = 1;
+    const SOUTH = 2;
+    const WEST = 3;
+}
 
-// Inititalisation des coordonnées
-$pj->x=0;
-$pj->y=0;
+class Personnage {
+    private $pos;
+    function Personnage() {
+        $this->pos = new Emplacement(0, 0);
+    }
+    function aller($direction) {
+        $this->pos = $this->pos->getVoisin($direction);
+        /*
+        switch($direction) {
+            case Direction::NORTH:
+                $this->y++;
+            break;
+            case Direction::EAST:
+                $this->x++;
+            break;
+            case Direction::SOUTH:
+                $this->y--;
+            break;
+            case Direction::WEST:
+                $this->x--;
+            break;
+            default:
+                //ERROR
+        }*/
+    }
 
-// Position initiale
-$pj->afficherPosition();
-    
-$pj->monter();
-$pj->afficherPosition();
-    
-$pj->droite();
-$pj->droite();
-$pj->afficherPosition();
 
-echo '<h4>Objet en cours</h4>';
-print_r($pj);
+    function getX() {
+        return $this->pos->getX();
+    }
+    function getY() {
+        return $this->pos->getY();
+    }
+}
 
+class Util {
+    static function affichePersonnagePosition($p) {
+        echo "Le personnage se situe en (" . $p->getX() . ", " . $p->getY() . ")<br/>\n";
+    }
+}
+$pj = new Personnage(); // Initialisation 0,0
+Util::affichePersonnagePosition($pj);
+$pj->aller(Direction::EAST);
+//assert $pj->getX() == 1;
+//assert $pj->getY() == 0;
+Util::affichePersonnagePosition($pj);
+
+$pj->aller(Direction::EAST);
+//assert $pj->getX() == 2;
+//assert $pj->getY() == 0;
+Util::affichePersonnagePosition($pj);
 
 
 ?>            
