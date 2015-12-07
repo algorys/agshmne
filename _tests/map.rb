79ec83e4fca595ab5925457f2e3d71ce1a7b53b0
Matthieu Estrada <m.estrada@alpi.fr>
2015-12-07 17:33:47 +0100

    Séparation des class dans plusieurs fichiers

--- _tests/map.rb
+++ _tests/map.rb
@@ -1,74 +1,9 @@
 require_relative 'utils'
-
+require_relative 'personnage'
 <<-DOC
 OBJECTIF
  Créer un personnage qui se déplace.
 DOC
-
-# @Position : Définit la direction du personnage et ses coordonnées
-# @pos correspond à l'objet "coordonnees"
-#
-class Position
-    private
-    def initialize *args
-        case args.size
-        when 0
-            @pos = 0
-        when 1
-            @pos = args[0]
-        when 2
-            @pos = 5*args[1] + args[0]
-        end
-    end
-    public
-    def move(direction)
-        case direction
-            when "NORTH"
-                @pos += 5
-            when "EAST"
-                @pos += 1
-            when "SOUTH"
-                @pos -= 5
-            when "WEST"
-                @pos -= 1
-            #TODO Gerer ERROR
-                # utiliser raise
-            else
-                puts "Vous vous êtes trompé de direction !"
-        end
-    end
-    def get_x()
-        @pos % 5
-    end
-    def get_y()
-        @pos / 5
-    end
-    def get_pos()
-        @pos
-    end
-end
-
-# Personnage : Définit le personnage
-# TODO : Déplacer cet objet dans un fichier à part
-#
-class Personnage
-    private
-    def initialize()
-        @vital = {'vie'=>0,'mana'=>0}
-        @carac = {'puissance'=>0,'defense'=>0}
-        @position = Position.new
-    end
-    public
-    def go_to(direction)
-        @position.move(direction)
-    end
-    def get_x()
-        @position.get_x
-    end
-    def get_y()
-        @position.get_y
-    end
-end
 
 class TypeTerrain
     # TODO Fusionner avec Terrain
