37cad838634b4567cf7056ac9dff4db0a7625635
Matthieu Estrada <m.estrada@alpi.fr>
2015-12-18 16:15:35 +0100

    Mise en place de la gestion de Position

---
+++ _tests/java/src/agshmne/Position.java
@@ -0,0 +1,30 @@
+package agshmne;
+
+public class Position {
+	public static final int MAX_X = 2;
+	public static final int MIN_X = -2;
+	public static final int MAX_Y = 2;
+	public static final int MIN_Y = -2;
+
+	private final int x;
+	private final int y;
+
+	public Position(int x, int y) {
+		if (x < MIN_X || x > MAX_X || y < MIN_Y || y > MAX_Y) {
+			throw new IllegalArgumentException(
+					String.format("(x,y) devrait être compris entre (%d, %d) et (%d, %d) mais (%d,%d) reçu.", MIN_X,
+							MIN_Y, MAX_X, MAX_Y, x, y));
+		}
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
--- _tests/java/src/agshmne/Region.java
+++ _tests/java/src/agshmne/Region.java
@@ -3,18 +3,26 @@ package agshmne;
 import java.util.Arrays;
 
 public class Region {
-	private Tile[] tiles = new Tile[25];
+	private final static int WIDTH = (Position.MAX_X - Position.MIN_X + 1);
+	private final static int HEIGHT = (Position.MAX_Y - Position.MIN_Y + 1);
+	private final static int SIZE = WIDTH * HEIGHT;
+	private Tile[] tiles = new Tile[SIZE];
 
 	public Region() {
-		for (int i = 0; i < 25; i++) {
+		for (int i = 0; i < SIZE; i++) {
 			TileType type = TileType.values()[agshmne.Outils.dice(TileType.values().length)];
 			tiles[i] = new Tile(type);
-			while(Math.random() < .5) {
+			while (Math.random() < .5) {
 				tiles[i].addItem(new Apple());
 			}
 		}
 	}
-	
+
+	public Tile getTileFromPosition(Position p) {
+		int z = (p.getY() - Position.MIN_Y) * WIDTH + (p.getX() - Position.MIN_X);
+		return tiles[z];
+	}
+
 	@Override
 	public String toString() {
 		return Arrays.toString(tiles);
