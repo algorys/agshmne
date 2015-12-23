9b7aa5d849f58b55aeaa19d1049a46f4826e2362
Matthieu Estrada <m.estrada@alpi.fr>
2015-12-23 17:16:57 +0100

    Mise au propre des fichiers

--- _tests/java/src/io/github/algorys/agshmne/game/Campagne.java
+++ _tests/java/src/io/github/algorys/agshmne/game/Campagne.java
@@ -1,6 +1,7 @@
 package io.github.algorys.agshmne.game;
 
 import io.github.algorys.agshmne.deplacement.Direction;
+import io.github.algorys.agshmne.fenetre.Fenetre;
 import io.github.algorys.agshmne.outils.MapUtils;
 import io.github.algorys.agshmne.personnage.Personnage;
 import io.github.algorys.agshmne.region.Region;
@@ -17,9 +18,13 @@ public class Campagne {
 	}
 
 	public void run() {
-		world.move(Direction.NORTH, pj);
-		mapUtils.showStatus(pj);
-		world.move(Direction.NORTH, pj);
-		mapUtils.showStatus(pj);
+		//world.move(Direction.NORTH, pj);
+		//mapUtils.showStatus(pj);
+		//world.move(Direction.NORTH, pj);
+		//mapUtils.showStatus(pj);
+		
+		// TEST FENETRE
+		Fenetre fenetre = new Fenetre(world);
+		fenetre.setVisible(true);
 	}
 }
--- _tests/java/src/io/github/algorys/agshmne/personnage/Personnage.java
+++ _tests/java/src/io/github/algorys/agshmne/personnage/Personnage.java
@@ -26,44 +26,5 @@ public class Personnage {
 	public void setPosition(Position position) {
 		this.position = position;
 	}
-//	public void move(Direction direction) {
-//		switch (direction) {
-//		case NORTH:
-//			if(this.position.getY() < Position.MAX_Y) {
-//				this.position = new Position(this.position.getX(), this.position.getY() + 1);
-//			} else {
-//				// changer de region (si possible?)
-//				// partir du bas de la region
-//				System.out.println("Vous êtes au bord du monde !");
-//			}
-//			break;
-//		case EAST:
-//			if(this.position.getX() < Position.MAX_X) {
-//				this.position = new Position(this.position.getX() + 1, this.position.getY());
-//			} else {
-//				// changer de region (si possible?)
-//				// partir de l'ouest de la region
-//				System.out.println("Vous êtes au bord du monde !");
-//			}
-//			break;
-//		case SOUTH:
-//			if(this.position.getY() > Position.MIN_Y) {
-//				this.position = new Position(this.position.getX(), this.position.getY() -1);
-//			} else {
-//				// changer de region (si possible?)
-//				// partir du haut de la region
-//				System.out.println("Vous êtes au bord du monde !");
-//			}
-//			break;
-//		case WEST:
-//			if(this.position.getX() > Position.MIN_X) {
-//				this.position = new Position(this.position.getX() -1, this.position.getY());
-//			} else {
-//				// changer de region (si possible?)
-//				// partir de l'est de la region
-//				System.out.println("Vous êtes au bord du monde !");
-//			}
-//			break;
-//		}
-	}
+}
 
