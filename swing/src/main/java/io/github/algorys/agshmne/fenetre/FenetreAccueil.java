7cfd69a69e6aa425ef42862ae2440e67b437d67e
Matthieu Estrada <m.estrada@alpi.fr>
2015-12-28 15:48:47 +0100

    abstractaction au lieu de actionlistener

--- swing/src/main/java/io/github/algorys/agshmne/fenetre/FenetreAccueil.java
+++ swing/src/main/java/io/github/algorys/agshmne/fenetre/FenetreAccueil.java
@@ -2,58 +2,81 @@ package io.github.algorys.agshmne.fenetre;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class FenetreAccueil extends JFrame{
-	    private JPanel cardPanel, jpAccueil, jpPerso, jpParams, buttonPanel;
-	    private JLabel jl1, jl2, jl3;
-	    private JButton btn1, btn2;
-	    private CardLayout cardLayout = new CardLayout();
+public class FenetreAccueil extends JFrame {
+	private static final String MAIN_MENU = "Main Menu";
+	private JPanel cardPanel;
+	private CardLayout cardLayout = new CardLayout();
+	private Action actionShowNewGame;
+	private Action actionShowSettings;
+	private Action actionMainMenu;
+	private JButton btLeft;
 
-	    public FenetreAccueil() {
-	        setTitle("Agshmne running...");
-	        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-	        
-	        cardPanel = new JPanel();
-	        buttonPanel = new JPanel();
-	        cardPanel.setLayout(cardLayout);
-	        jpAccueil = new JPanel();
-	        jpPerso = new JPanel();
-	        jpParams = new JPanel();
-	        jl1 = new JLabel("Accueil");
-	        jl2 = new JLabel("Création Personnage");
-	        jl3 = new JLabel("Paramètres");
-	        jpAccueil.add(jl1);
-	        jpPerso.add(jl2);
-	        jpParams.add(jl3);
-	        cardPanel.add(jpAccueil, "1");
-	        cardPanel.add(jpPerso, "2");
-	        cardPanel.add(jpParams, "3");
-	        btn1 = new JButton("Nouvelle Partie");
-	        btn1.addActionListener(new ActionListener() {
+	public FenetreAccueil() {
+		setTitle("Agshmne running...");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-	            public void actionPerformed(ActionEvent e) {
-	                cardLayout.show(cardPanel, "2");
-	            }
-	        });
-	        btn2 = new JButton("Paramètres");
-	        btn2.addActionListener(new ActionListener() {
+		cardPanel = new JPanel();
+		cardPanel.setLayout(cardLayout);
 
-	            public void actionPerformed(ActionEvent e) {
-	                cardLayout.show(cardPanel, "3");
-	            }
-	        });
-	        buttonPanel.add(btn1);
-	        buttonPanel.add(btn2);
-	        add(cardPanel, BorderLayout.CENTER);
-	        add(buttonPanel, BorderLayout.NORTH);
-	    }
-     
+		JPanel jpAccueil = new JPanel();
+		jpAccueil.add(new JLabel("Accueil"));
+
+		JPanel jpPerso = new JPanel();
+		jpPerso.add(new JLabel("Création Personnage"));
+
+		JPanel jpParams = new JPanel();
+		jpParams.add(new JLabel("Paramètres"));
+
+		cardPanel.add(jpAccueil, MAIN_MENU);
+		cardPanel.add(jpPerso, "2");
+		cardPanel.add(jpParams, "3");
+
+		btLeft = new JButton();
+		JButton btSettings = new JButton();
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(btLeft);
+		buttonPanel.add(btSettings);
+
+		add(cardPanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.NORTH);
+
+		actionShowNewGame = new AbstractAction("Nouvelle Partie") {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				cardLayout.show(cardPanel, "2");
+				btLeft.setAction(actionMainMenu);
+			}
+		};
+		actionMainMenu = new AbstractAction("Menu principal") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardLayout.show(cardPanel, MAIN_MENU);
+				btLeft.setAction(actionShowNewGame);
+			};
+		};
+		actionShowSettings = new AbstractAction("Paramètres") {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardLayout.show(cardPanel, "3");
+			}
+		};
+
+		btLeft.setAction(actionShowNewGame);
+		btSettings.setAction(actionShowSettings);
+		
+	}
+
 }
