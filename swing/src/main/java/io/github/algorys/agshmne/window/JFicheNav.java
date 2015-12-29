1db8b89243c224380ef2c3f31abdf655acf5d8d9
Estrada Matthieu <ttamalfor@gmail.com>
2015-12-29 23:33:48 +0100

    Ajout background temporaire pr fiche

--- swing/src/main/java/io/github/algorys/agshmne/window/JFicheNav.java
+++ swing/src/main/java/io/github/algorys/agshmne/window/JFicheNav.java
@@ -184,7 +184,7 @@ public class JFicheNav extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		try {
-			Image img = ImageIO.read(JTile.class.getClassLoader().getResource("parchemin.jpg"));
+			Image img = ImageIO.read(JTile.class.getClassLoader().getResource("parchemin.png"));
 			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 		} catch (IOException e) {
 			e.printStackTrace();
