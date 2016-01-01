a4450435e11de41376b1c362f8c2cac8a2bfc357
Estrada Matthieu <ttamalfor@gmail.com>
2016-01-01 09:48:31 +0100

    Ajout panneau Inventaire

---
+++ swing/src/main/java/io/github/algorys/agshmne/window/JInvPanel.java
@@ -0,0 +1,46 @@
+package io.github.algorys.agshmne.window;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+
+public class JInvPanel extends JLabel {
+	
+	public JInvPanel(){
+		// GidBag Layout
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbcInv = new GridBagConstraints();
+		// Ajuste la taille du GridBag
+		gbcInv.insets = new Insets(5, 5, 5, 5);
+	
+		// TITRE
+		gbcInv.gridy = 0;
+		gbcInv.gridheight = 1;
+		gbcInv.gridx = 0;
+		gbcInv.gridwidth = 2;
+		gbcInv.anchor = GridBagConstraints.CENTER;
+		gbcInv.fill = GridBagConstraints.NONE;
+		this.add(new JLabel("FICHE PERSONNAGE JOUEUR"), gbcInv);
+		
+		// Titre Panel
+		gbcInv.gridy = 1;
+		gbcInv.gridheight = 1;
+		gbcInv.gridx = 0;
+		gbcInv.gridwidth = 2;
+		gbcInv.anchor = GridBagConstraints.CENTER;
+		gbcInv.fill = GridBagConstraints.NONE;
+		this.add(new JLabel("- Inventaire -"), gbcInv);
+	
+		// POINTS
+		gbcInv.gridy = 2;
+		gbcInv.gridheight = 1;
+		gbcInv.gridx = 0;
+		gbcInv.gridwidth = 1;
+		gbcInv.anchor = GridBagConstraints.EAST;
+		gbcInv.fill = GridBagConstraints.NONE;
+		this.add(new JLabel("Vous êtes trop pauvre !"), gbcInv);
+	
+	}
+}
