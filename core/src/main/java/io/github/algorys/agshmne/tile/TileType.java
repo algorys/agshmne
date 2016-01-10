dbc08089ec5533585cec571f5bb827362d551e43
Estrada Matthieu <ttamalfor@gmail.com>
2016-01-10 14:53:50 +0100

    Ajout Valley typeTile

--- core/src/main/java/io/github/algorys/agshmne/tile/TileType.java
+++ core/src/main/java/io/github/algorys/agshmne/tile/TileType.java
@@ -1,5 +1,5 @@
 package io.github.algorys.agshmne.tile;
 
 public enum TileType {
-	Wood, Hill, Desert, Sea, Forest, Mountain, Plain, River, Swamp, Lake
+	Wood, Hill, Desert, Sea, Forest, Mountain, Plain, River, Swamp, Lake, Valley
 }
