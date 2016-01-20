ee4d5627cdb9fcb198787d824a2389da2e7e1120
Matthieu Estrada <m.estrada@alpi.fr>
2016-01-20 11:48:15 +0100

    Suppression des imports inutiles

--- core/src/main/java/io/github/algorys/agshmne/city/factory/CityCitadelFactory.java
+++ core/src/main/java/io/github/algorys/agshmne/city/factory/CityCitadelFactory.java
@@ -2,7 +2,6 @@ package io.github.algorys.agshmne.city.factory;
 
 import io.github.algorys.agshmne.city.City;
 import io.github.algorys.agshmne.city.ICityFactory;
-import io.github.algorys.agshmne.city.CityType;
 import io.github.algorys.agshmne.tools.Tools;
 
 public class CityCitadelFactory implements ICityFactory {
