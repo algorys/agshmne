abfc82d38986b306a3ad80984648ecd09993770d
Estrada Matthieu <ttamalfor@gmail.com>
2016-01-21 23:20:40 +0100

    Ajout interface pour objet de quêtes# Veuillez saisir le message de validation pour vos modifications. Les lignes

---
+++ core/src/main/java/io/github/algorys/agshmne/inventory/QuestItem.java
@@ -0,0 +1,12 @@
+package io.github.algorys.agshmne.inventory;
+
+import io.github.algorys.agshmne.movement.Position;
+
+public interface QuestItem extends InventoryItem {
+	
+	public Position getDestination();
+	
+	public Position setDestination(Position destination);
+	
+	public int getLevel();
+}
