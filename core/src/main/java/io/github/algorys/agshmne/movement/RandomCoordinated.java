4424bbf7a0ed47b1608ae584c62b0e43a24a47d5
Matthieu Estrada <m.estrada@alpi.fr>
2016-01-22 11:18:59 +0100

    Ajout de coordonnées aléatoires

---
+++ core/src/main/java/io/github/algorys/agshmne/movement/RandomCoordinated.java
@@ -0,0 +1,19 @@
+package io.github.algorys.agshmne.movement;
+
+import java.util.Random;
+
+public class RandomCoordinated {
+	private int coordinated;
+	
+	public RandomCoordinated(int max, int min) {
+		int range = max - min + 1;
+		Random rand = new Random();
+		coordinated = rand.nextInt(range) + min;
+	}
+
+	public int getCoordinated() {
+		return coordinated;
+	}
+	
+	
+}
