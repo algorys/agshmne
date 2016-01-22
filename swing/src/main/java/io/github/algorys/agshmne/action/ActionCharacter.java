915bee6a09782089102dacee21a80eee2205b7a2
Matthieu Estrada <m.estrada@alpi.fr>
2016-01-22 16:42:07 +0100

    Externalisation des Actions pour competences

--- swing/src/main/java/io/github/algorys/agshmne/action/ActionCharacter.java
+++ swing/src/main/java/io/github/algorys/agshmne/action/ActionCharacter.java
@@ -1,8 +1,6 @@
 package io.github.algorys.agshmne.action;
 
 import java.awt.event.ActionEvent;
-import java.util.Observable;
-import java.util.Observer;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -11,66 +9,33 @@ import javax.swing.JPanel;
 
 import io.github.algorys.agshmne.character.Character;
 import io.github.algorys.agshmne.items.craft.lowOre;
-import io.github.algorys.agshmne.items.craft.lowWood;
-import io.github.algorys.agshmne.tile.Tile;
 import io.github.algorys.agshmne.tile.TileType;
 
-public class ActionCharacter extends JPanel implements Observer {
-	JButton woodButton;
-	JButton oreButton;
-	private Action cutWood;
-	private Action mineOre;
-	private final Character pj;
-	private Tile currentTile;
-	
+public class ActionCharacter extends JPanel {
 	public ActionCharacter(final Character pj){
-		this.pj = pj;
-		pj.addObserver(this);
-		
 		// Boutons d'action
-		woodButton = new JButton("Couper du bois");
-		oreButton = new JButton("Récolter Minerai");
+		JButton woodButton = new JButton("Couper du bois");
+		JButton oreButton = new JButton("Récolter Minerai");
 		this.add(woodButton);
 		this.add(oreButton);
 		
-		cutWood = new AbstractAction("Couper du bois") {
-
-			@Override
-			public void actionPerformed(ActionEvent wood) {
-				System.out.println("Bois coupé !");	
-				pj.getInventory().addItem(new lowWood());
-			}
-			
-		};
-		woodButton.setAction(cutWood);
+		woodButton.setAction(new CompetenceCutAction(pj));
 		
-		mineOre = new AbstractAction("Récolter Minerai") {
+		Action mineOre = new AbstractAction("Récolter Minerai") {
 
 			@Override
 			public void actionPerformed(ActionEvent ore) {
+				if(pj.getTile().getType() == TileType.Mountain) {
+					this.setEnabled(true);
+				} else {
+					this.setEnabled(false);
+				}
+				
 				System.out.println("Minerai trouvé !");	
 				pj.getInventory().addItem(new lowOre());
 			}
 			
 		};
 		oreButton.setAction(mineOre);
-		
-		this.update(pj, null);
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		this.currentTile = pj.getRegion().getTileFromPosition(pj.getPosition());
-		if(this.currentTile.getType() == TileType.Wood || this.currentTile.getType() == TileType.Forest) {
-			woodButton.setEnabled(true);
-		} else {
-			this.woodButton.setEnabled(false);
-		}
-		if(this.currentTile.getType() == TileType.Mountain) {
-			oreButton.setEnabled(true);
-		} else {
-			this.oreButton.setEnabled(false);
-		}
-		this.currentTile.addObserver(this);
 	}
 }
---
+++ swing/src/main/java/io/github/algorys/agshmne/action/CompetenceCutAction.java
@@ -0,0 +1,39 @@
+package io.github.algorys.agshmne.action;
+
+import java.awt.event.ActionEvent;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.AbstractAction;
+
+import io.github.algorys.agshmne.character.Character;
+import io.github.algorys.agshmne.items.craft.lowWood;
+
+final class CompetenceCutAction extends AbstractAction implements Observer {
+	private final Character pj;
+
+	public CompetenceCutAction(Character pj) {
+		super("Couper du bois");
+		this.pj = pj;
+		pj.addObserver(this);
+		this.update(pj, null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent wood) {
+		System.out.println("Bois coupé !");
+		pj.getInventory().addItem(new lowWood());
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		switch (pj.getTile().getType()) {
+		case Wood:
+		case Forest:
+			this.setEnabled(true);
+			break;
+		default:
+			this.setEnabled(false);
+		}
+	}
+}
