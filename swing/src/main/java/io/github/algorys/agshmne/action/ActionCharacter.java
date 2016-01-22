41e6a04e238e70a49fe0d05df10fa28fe89c8df4
Matthieu Estrada <m.estrada@alpi.fr>
2016-01-22 15:51:27 +0100

    Test pour action mineOre et Minerai

--- swing/src/main/java/io/github/algorys/agshmne/action/ActionCharacter.java
+++ swing/src/main/java/io/github/algorys/agshmne/action/ActionCharacter.java
@@ -10,41 +10,66 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import io.github.algorys.agshmne.character.Character;
-import io.github.algorys.agshmne.region.Region;
+import io.github.algorys.agshmne.items.craft.lowWood;
 import io.github.algorys.agshmne.tile.Tile;
 import io.github.algorys.agshmne.tile.TileType;
 
 public class ActionCharacter extends JPanel implements Observer {
 	JButton woodButton;
+	JButton oreButton;
 	private Action cutWood;
-	private Character pj;
+	private Action mineOre;
+	private final Character pj;
 	private Tile currentTile;
 	
-	public ActionCharacter(Character pj){
+	public ActionCharacter(final Character pj){
 		this.pj = pj;
 		pj.addObserver(this);
-		woodButton = new JButton("Couper du bois");		
+		
+		// Boutons d'action
+		woodButton = new JButton("Couper du bois");
+		oreButton = new JButton("Récolter Minerai");
 		this.add(woodButton);
-		this.update(pj, null);
+		this.add(oreButton);
 		
 		cutWood = new AbstractAction("Couper du bois") {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("Bois coupé !");				
+				System.out.println("Bois coupé !");	
+				pj.getInventory().addItem(new lowWood());
 			}
 			
 		};
 		woodButton.setAction(cutWood);
+		
+		mineOre = new AbstractAction("Récolter Minerai") {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Minerai trouvé !");	
+				pj.getInventory().addItem(new lowWood());
+			}
+			
+		};
+		oreButton.setAction(mineOre);
+		
+		this.update(pj, null);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		this.currentTile = pj.getRegion().getTileFromPosition(pj.getPosition());
-		if(this.currentTile.getType() == TileType.Wood) {
+		if(this.currentTile.getType() == TileType.Wood || this.currentTile.getType() == TileType.Forest) {
 			woodButton.setEnabled(true);
 		} else {
 			this.woodButton.setEnabled(false);
-		}		
+		}
+		if(this.currentTile.getType() == TileType.Mountain) {
+			oreButton.setEnabled(true);
+		} else {
+			this.oreButton.setEnabled(false);
+		}
+		this.currentTile.addObserver(this);
 	}
 }
