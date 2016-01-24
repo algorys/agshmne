17364a1287985cfea1cb4e459e21ea383c0be358
Estrada Matthieu <ttamalfor@gmail.com>
2016-01-24 10:10:42 +0100

    Ajout SearchRegion

---
+++ swing/src/main/java/io/github/algorys/agshmne/action/SkillSearchRegionAction.java
@@ -0,0 +1,33 @@
+package io.github.algorys.agshmne.action;
+
+import java.awt.event.ActionEvent;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.AbstractAction;
+
+import io.github.algorys.agshmne.character.Character;
+
+public class SkillSearchRegionAction extends AbstractAction implements Observer {
+	private final Character pj;
+	
+	public SkillSearchRegionAction(Character pj) {
+		super("Fouiller la Région");
+		this.pj = pj;
+		pj.addObserver(this);
+		this.update(pj, null);
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Vous fouillez la Région !");
+		// TODO Générer un évènement
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {	
+		// TODO Faire en sorte de savoir si la Tile a déjà été visitée ou pas.
+	}
+
+}
