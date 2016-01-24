540b5a554e9f1aee75c1796d6ee39806c8e3bc9e
Estrada Matthieu <ttamalfor@gmail.com>
2016-01-24 10:28:32 +0100

    Ajout SearchButton

--- swing/src/main/java/io/github/algorys/agshmne/action/ActionCharacter.java
+++ swing/src/main/java/io/github/algorys/agshmne/action/ActionCharacter.java
@@ -6,18 +6,22 @@ import javax.swing.JPanel;
 import io.github.algorys.agshmne.character.Character;
 
 public class ActionCharacter extends JPanel {
+	
 	public ActionCharacter(final Character pj){
 		// Boutons d'action
 		JButton woodButton = new JButton("Couper du Bois");
 		JButton oreButton = new JButton("Récolter Minerai");
-		JButton searchRegion = new JButton("Fouiller la Ville");
+		JButton searchCity = new JButton("Fouiller la Ville");
+		JButton searchRegion = new JButton("Fouiller la Région");
 		
 		this.add(woodButton);
 		this.add(oreButton);
 		this.add(searchRegion);
+		this.add(searchCity);
 		
 		woodButton.setAction(new SkillCutAction(pj));
 		oreButton.setAction(new SkillMineAction(pj));
-		searchRegion.setAction(new SkillSearchCityAction(pj));
+		searchRegion.setAction(new SkillSearchRegionAction(pj));
+		searchCity.setAction(new SkillSearchCityAction(pj));
 	}
 }
