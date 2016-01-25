12e7c191f5982a4709b8438f31572515a9ab7471
Matthieu Estrada <m.estrada@alpi.fr>
2016-01-25 17:30:09 +0100

    Exemple pour génération seulement de Fruits

--- core/src/main/java/io/github/algorys/agshmne/tools/Tools.java
+++ core/src/main/java/io/github/algorys/agshmne/tools/Tools.java
@@ -1,8 +1,19 @@
 package io.github.algorys.agshmne.tools;
 
+import java.util.Arrays;
+
+import io.github.algorys.agshmne.tempItem.ItemFactory;
+
 public class Tools {
 	public static int dice(int d){
 		int dice = (int)(Math.random()*d);
 		return dice;
+	}
+	
+	public ItemFactory getFruitFactory() {
+		ItemFactory myFruitFactory = new ItemFactory();
+		myFruitFactory.setSimpleItems(Arrays.asList("Ananas", "Rutabaga"));
+		myFruitFactory.setStackableItems(Arrays.asList("Apple", "Orange", "Abricot"));
+		return myFruitFactory;
 	}
 }
