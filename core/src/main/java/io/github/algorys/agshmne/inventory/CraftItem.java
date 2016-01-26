5d7dda3e9aa15ebf35aabc774490f63cef47927d
Matthieu Estrada <m.estrada@alpi.fr>
2016-01-26 15:28:49 +0100

    Suppression de l'ancien Inventaire

--- core/src/main/java/io/github/algorys/agshmne/inventory/CraftItem.java
+++
@@ -1,7 +0,0 @@
-package io.github.algorys.agshmne.inventory;
-
-public interface CraftItem extends InventoryItem {
-	// TODO cette interface sera utilisé pour pouvoir avoir la qualité de l'Item.
-	
-//	public int getQuality();
-}
--- core/src/main/java/io/github/algorys/agshmne/inventory/InventoryItem.java
+++
@@ -1,8 +0,0 @@
-package io.github.algorys.agshmne.inventory;
-
-public interface InventoryItem {
-	public String getName();
-
-	public int getWeight();
-	
-}
