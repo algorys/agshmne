d1609d23fd26b1f88ac9346639411d6f627636b1
Estrada Matthieu <ttamalfor@gmail.com>
2016-01-26 23:30:48 +0100

    Ajout tests Unitaires pr Opponent / beast #21

---
+++ core/src/test/java/io/github/algorys/agshmne/opponent/OpponentTest.java
@@ -0,0 +1,32 @@
+package io.github.algorys.agshmne.opponent;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.github.algorys.agshmne.opponent.beast.Beast;
+import io.github.algorys.agshmne.opponent.beast.BeastFactory;
+
+public class OpponentTest {
+
+	@Test
+	public void oneBeastShouldHaveAttributes() {
+		BeastFactory beastFact = new BeastFactory();
+		Beast wolf = beastFact.createBeast();
+		assertTrue(wolf.getAttributes()!= null);
+	}
+	
+	@Test
+	public void oneBeastShouldHaveALevel() {
+		BeastFactory beastFact = new BeastFactory();
+		Beast wolf = beastFact.createBeast();
+		assertTrue(wolf.getLevel() > 0);
+	}
+	
+	@Test
+	public void beastFactShouldCreateABeast() {
+		BeastFactory beastFact = new BeastFactory();
+		assertTrue(beastFact.createBeast() instanceof Beast);
+	}
+
+}
