c10bc62915c25402fdc7375a274839d2749b23a8
Isammoc OFF <isammoc@gmail.com>
2016-01-27 10:05:29 +0100

    Construit Beast sans logique
    La `BeastAttributeFactory` appartient à la `BeastFactory`.

--- core/src/main/java/io/github/algorys/agshmne/opponent/beast/BeastFactory.java
+++ core/src/main/java/io/github/algorys/agshmne/opponent/beast/BeastFactory.java
@@ -7,18 +7,20 @@ import io.github.algorys.agshmne.tools.Tools;
 
 public class BeastFactory {
 	private List<String> lowMonster = new ArrayList<>();
-	
+	private final BeastAttributeFactory beastAttributeFactory = new BeastAttributeFactory();
+
 	public BeastFactory() {
 		lowMonster.add("Wolf:3");
 		lowMonster.add("Spider:2");
 		lowMonster.add("Vermin:1");
 		lowMonster.add("Jelly:1");
-		lowMonster.add("Goblins:3");		
+		lowMonster.add("Goblins:3");
 	}
-	
+
 	public Beast createBeast() {
 		String line = lowMonster.get(Tools.dice(lowMonster.size()));
 		String[] parts = line.split(":");
-		return new Beast(parts[0], Integer.parseInt(parts[1]));
+		int level = Integer.parseInt(parts[1]);
+		return new Beast(parts[0], level, beastAttributeFactory.createAttributes(level));
 	}
 }
