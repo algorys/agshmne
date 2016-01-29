cdb9191b036e96bd80f30610dcd3043eb8dcda2d
Matthieu Estrada <ttamalfor@gmail.com>
2016-01-29 12:19:56 +0100

    Ajout fichier main de test pr JCharacter

---
+++ swing/src/test/java/io/github/algorys/agshmne/game/JCharacterTest.java
@@ -0,0 +1,28 @@
+package io.github.algorys.agshmne.game;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import io.github.algorys.agshmne.character.player.Player;
+import io.github.algorys.agshmne.game.south.JCharacter;
+import io.github.algorys.agshmne.map.region.Region;
+import io.github.algorys.agshmne.map.tile.RandomTileFactory;
+
+public class JCharacterTest {
+
+	public static void main(String[] args) {
+		JFrame jf = new JFrame();
+		jf.setSize(300, 300);
+		jf.setBackground(Color.black);
+		Player pj = new Player(new Region(new RandomTileFactory()));
+		pj.getSocial().setName("Player");
+		pj.initVital();
+		JCharacter jc = new JCharacter(pj);
+		jc.setOpaque(true);
+		jf.add(jc);
+		
+		jf.setVisible(true);
+	}
+
+}
