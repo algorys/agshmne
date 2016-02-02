e956ca80aa790c193b4425085eae35780c9159ea
Matthieu Estrada <ttamalfor@gmail.com>
2016-02-02 10:28:47 +0100

    Suppression paramètre inutile

--- core/src/main/java/io/github/algorys/agshmne/events/AdventureFactory.java
+++ core/src/main/java/io/github/algorys/agshmne/events/AdventureFactory.java
@@ -7,15 +7,12 @@ import io.github.algorys.agshmne.character.opponent.beast.BeastFactory;
 import io.github.algorys.agshmne.character.player.Player;
 import io.github.algorys.agshmne.events.fight.Fight;
 import io.github.algorys.agshmne.events.quest.QuestFactory;
-import io.github.algorys.agshmne.map.tile.Tile;
 
 public class AdventureFactory {
 	private Map<AdventureType, IAdventure> factories =  new HashMap<>();
-	private Tile tile;
 	private Player pj;
 
-	public AdventureFactory(Tile tile, Player pj) {
-		this.tile = tile;
+	public AdventureFactory(Player pj) {
 		this.pj = pj;
 		IAdventure fight = new Fight(pj, new BeastFactory().createBeast());
 		IAdventure quest = new QuestFactory(pj);
