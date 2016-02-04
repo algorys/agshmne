427b5fc6ca0125c0bda86e43c34907c36d65af73
Matthieu Estrada <ttamalfor@gmail.com>
2016-02-04 10:46:21 +0100

    Ajout TU pr différents types d'Item

--- core/src/test/java/io/github/algorys/agshmne/map/city/shop/ShopTest.java
+++ core/src/test/java/io/github/algorys/agshmne/map/city/shop/ShopTest.java
@@ -11,6 +11,9 @@ import io.github.algorys.agshmne.character.player.Player;
 import io.github.algorys.agshmne.items.GeneralItem;
 import io.github.algorys.agshmne.items.Inventory;
 import io.github.algorys.agshmne.items.Item;
+import io.github.algorys.agshmne.items.equipable.BodyPart;
+import io.github.algorys.agshmne.items.equipable.EquipableItem;
+import io.github.algorys.agshmne.items.stackable.StackableItem;
 
 public class ShopTest {
 
@@ -21,9 +24,9 @@ public class ShopTest {
 	}
 	
 	@Test
-	public void newShopCouldSellAnItem() {
+	public void newShopCouldSellAnyItem() {
 		Shop underTest = new Shop(1);
-		Item item = new GeneralItem("Pomme", 2);
+		Item item = new StackableItem("Pomme", 1, 2);
 		underTest.getInventory().addItem(item);
 		Player pj = new Player(null);
 		Inventory old = underTest.getInventory();
@@ -35,9 +38,65 @@ public class ShopTest {
 	}
 	
 	@Test
-	public void newShopCanBuyAnItem() {
+	public void newShopCouldSellStackableItem() {
 		Shop underTest = new Shop(1);
-		Item item = new GeneralItem("Pomme", 2);
+		StackableItem item = new StackableItem("Pomme", 1, 2);
+		underTest.getInventory().addItem(item);
+		Player pj = new Player(null);
+		Inventory old = underTest.getInventory();
+	
+		underTest.sellItem(pj, item);
+		assertNotSame(old.getListBackpack(), underTest.getInventory().getListBackpack());
+		assertFalse(underTest.getInventory().contains(item));
+		assertTrue(pj.getInventory().contains(item));
+	}
+	
+	@Test
+	public void shopCanSellEquipableItem() {
+		Shop underTest = new Shop(1);
+		EquipableItem item = new EquipableItem("Chapeau", BodyPart.HEAD, 15);
+		underTest.getInventory().addItem(item);
+		Player pj = new Player(null);
+		Inventory old = underTest.getInventory();
+	
+		underTest.sellItem(pj, item);
+		assertNotSame(old.getListBackpack(), underTest.getInventory().getListBackpack());
+		assertFalse(underTest.getInventory().contains(item));
+		assertTrue(pj.getInventory().contains(item));
+	}
+	
+	@Test
+	public void shopCanSellGeneralItem() {
+		Shop underTest = new Shop(1);
+		GeneralItem item = new GeneralItem("Bouteille", 3);
+		underTest.getInventory().addItem(item);
+		Player pj = new Player(null);
+		Inventory old = underTest.getInventory();
+	
+		underTest.sellItem(pj, item);
+		assertNotSame(old.getListBackpack(), underTest.getInventory().getListBackpack());
+		assertFalse(underTest.getInventory().contains(item));
+		assertTrue(pj.getInventory().contains(item));
+	}
+	
+	@Test
+	public void newShopCanBuyStackableItem() {
+		Shop underTest = new Shop(1);
+		StackableItem item = new StackableItem("Pomme", 1, 2);
+		Player pj = new Player(null);
+		pj.getInventory().addItem(item);
+		Inventory old = underTest.getInventory();
+	
+		underTest.buyItem(pj, item);
+		assertNotSame(old.getListBackpack(), underTest.getInventory().getListBackpack());
+		assertTrue(underTest.getInventory().contains(item));
+		assertFalse(pj.getInventory().contains(item));
+	}
+	
+	@Test
+	public void newShopCanBuyEquipableItem() {
+		Shop underTest = new Shop(1);
+		EquipableItem item = new EquipableItem("Armure de cuir", BodyPart.CHEST, 2);
 		Player pj = new Player(null);
 		pj.getInventory().addItem(item);
 		Inventory old = underTest.getInventory();
