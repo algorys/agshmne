d3e6135672cd69230d5cb0e34194ebb0e9f4fd71
Estrada Matthieu <ttamalfor@gmail.com>
2016-02-07 10:23:28 +0100

    Refonte graphique JPanelRight

--- swing/src/main/java/io/github/algorys/agshmne/game/actions/ActionCharacter.java
+++ swing/src/main/java/io/github/algorys/agshmne/game/actions/ActionCharacter.java
@@ -1,6 +1,8 @@
 package io.github.algorys.agshmne.game.actions;
 
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -11,7 +13,10 @@ import io.github.algorys.agshmne.character.player.Player;
 public class ActionCharacter extends JPanel {
 	
 	public ActionCharacter(final Player pj){
-		this.setLayout(new GridLayout(0,1));
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbcActions = new GridBagConstraints();
+		gbcActions.insets = new Insets(5, 5, 5, 5);
+		
 		// Boutons d'action
 		JButton woodButton = new JButton("Couper du Bois");
 		JButton oreButton = new JButton("Récolter Minerai");
@@ -20,11 +25,45 @@ public class ActionCharacter extends JPanel {
 		JButton searchCity = new JButton("Fouiller la Ville");
 		JButton searchRegion = new JButton("Fouiller la Région");
 		
-		this.add(woodButton);
-		this.add(oreButton);
-		this.add(fishButton);
-		this.add(searchRegion);
-		this.add(searchCity);
+		gbcActions.gridy = 0;
+		gbcActions.gridheight = 1;
+		gbcActions.gridx = 0;
+		gbcActions.gridwidth = 4;
+		gbcActions.anchor = GridBagConstraints.CENTER;
+		gbcActions.fill = GridBagConstraints.HORIZONTAL;
+		this.add(woodButton, gbcActions);
+		
+		gbcActions.gridy = 1;
+		gbcActions.gridheight = 1;
+		gbcActions.gridx = 0;
+		gbcActions.gridwidth = 4;
+		gbcActions.anchor = GridBagConstraints.CENTER;
+		gbcActions.fill = GridBagConstraints.HORIZONTAL;
+		this.add(oreButton, gbcActions);
+		
+		gbcActions.gridy = 2;
+		gbcActions.gridheight = 1;
+		gbcActions.gridx = 0;
+		gbcActions.gridwidth = 4;
+		gbcActions.anchor = GridBagConstraints.CENTER;
+		gbcActions.fill = GridBagConstraints.HORIZONTAL;
+		this.add(fishButton, gbcActions);
+		
+		gbcActions.gridy = 3;
+		gbcActions.gridheight = 1;
+		gbcActions.gridx = 0;
+		gbcActions.gridwidth = 4;
+		gbcActions.anchor = GridBagConstraints.CENTER;
+		gbcActions.fill = GridBagConstraints.HORIZONTAL;
+		this.add(searchRegion, gbcActions);
+		
+		gbcActions.gridy = 4;
+		gbcActions.gridheight = 1;
+		gbcActions.gridx = 0;
+		gbcActions.gridwidth = 4;
+		gbcActions.anchor = GridBagConstraints.CENTER;
+		gbcActions.fill = GridBagConstraints.HORIZONTAL;
+		this.add(searchCity, gbcActions);
 		
 		woodButton.setAction(new SkillCutAction(pj));
 		oreButton.setAction(new SkillMineAction(pj));
--- swing/src/main/java/io/github/algorys/agshmne/game/mainpanel/JPanRight.java
+++ swing/src/main/java/io/github/algorys/agshmne/game/mainpanel/JPanRight.java
@@ -2,14 +2,15 @@ package io.github.algorys.agshmne.game.mainpanel;
 
 import java.awt.Dimension;
 
-import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import io.github.algorys.agshmne.character.player.Player;
 import io.github.algorys.agshmne.game.actions.ActionCharacter;
 import io.github.algorys.agshmne.tile.JTileInventory;
 
 @SuppressWarnings("serial")
-public class JPanRight extends JTabbedPane {
+public class JPanRight extends JPanel {
 	private JTileInventory grdInv;
 	private ActionCharacter actionCharacter;
 	
@@ -18,8 +19,12 @@ public class JPanRight extends JTabbedPane {
 		grdInv = new JTileInventory(pj);
 		actionCharacter = new ActionCharacter(pj);
 		
-		this.addTab("Objets à terre", grdInv);
+		JLabel jlGround = new JLabel("Objets au sol : ");
+		this.add(jlGround);
+		this.add("Objets à terre", grdInv);
 		
-		this.addTab("Actions", actionCharacter);
+		JLabel jlAction = new JLabel("Actions générales");
+		this.add(jlAction);
+		this.add("Actions", actionCharacter);
 	}
 }
