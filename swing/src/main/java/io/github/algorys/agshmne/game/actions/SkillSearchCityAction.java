ef68e223fc3f73db3bd0028e9cd0a97b70851647
Matthieu Estrada <ttamalfor@gmail.com>
2016-02-11 16:49:57 +0100

    Fouiller la Ville permet de trouver des objets
    Fix #54

--- swing/src/main/java/io/github/algorys/agshmne/game/actions/SkillSearchCityAction.java
+++ swing/src/main/java/io/github/algorys/agshmne/game/actions/SkillSearchCityAction.java
@@ -1,12 +1,20 @@
 package io.github.algorys.agshmne.game.actions;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 import io.github.algorys.agshmne.character.player.Player;
+import io.github.algorys.agshmne.character.player.skills.SkillTool;
+import io.github.algorys.agshmne.character.player.skills.SkillType;
+import io.github.algorys.agshmne.items.Item;
+import io.github.algorys.agshmne.items.ItemFactory;
 
 @SuppressWarnings("serial")
 public class SkillSearchCityAction extends AbstractAction implements PropertyChangeListener {
@@ -22,7 +30,17 @@ public class SkillSearchCityAction extends AbstractAction implements PropertyCha
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Vous fouillez la Ville !");
-		// TODO Faire en sorte qu'on puisse trouver des choses dans une ville
+		if (SkillTool.Dice(pj.getSkills().getSkillLevel(SkillType.fouiller), 10)) {
+			Item item = new ItemFactory().createRandom();
+			pj.getInventory().addItem(item);
+			JFrame topFrame = null;
+			if (e.getSource() instanceof Component) {
+				Component sourceComponent = (Component) e.getSource();
+				topFrame = (JFrame) SwingUtilities.getWindowAncestor(sourceComponent);
+			}
+			JOptionPane.showMessageDialog(topFrame, "Vous avez trouvé quelque chose : " + item.getName());
+		}
+		this.setEnabled(false);
 	}
 
 	@Override
