c05985a2ef7b3f4cc256983c53b06e6ab56a9fa1
Isammoc OFF <isammoc@gmail.com>
2016-02-16 19:06:09 +0100

    JPanMain n'écoute plus Position mais Tile

--- swing/src/main/java/io/github/algorys/agshmne/game/mainpanel/JPanMain.java
+++ swing/src/main/java/io/github/algorys/agshmne/game/mainpanel/JPanMain.java
@@ -18,9 +18,10 @@ import io.github.algorys.agshmne.game.quest.JPanQuest;
 import io.github.algorys.agshmne.map.JMapRegion;
 import io.github.algorys.agshmne.map.city.JPanCity;
 import io.github.algorys.agshmne.map.desc.JMapDesc;
+import io.github.algorys.agshmne.map.tile.Tile;
 
 @SuppressWarnings("serial")
-public class JPanMain extends JTabbedPane implements PropertyChangeListener {
+public class JPanMain extends JTabbedPane {
 	public static final int MAP_PANEL_INDEX = 0;
 	public static final int CITY_PANEL_INDEX = 6;
 	private JPanel jpMap = new JPanel();
@@ -33,46 +34,50 @@ public class JPanMain extends JTabbedPane implements PropertyChangeListener {
 	public JPanMain(final JMapRegion jregion) {
 		this.setTabPlacement(JTabbedPane.TOP);
 		this.setPreferredSize(new Dimension(1100, 1000));
-		
+
 		// Carte
-		jregion.getPersonnage().addPropertyChangeListener(Player.PROPERTY_POSITION, this);
+		Player pj = jregion.getPersonnage();
+		pj.addPropertyChangeListener(Player.PROPERTY_TILE, new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				if (evt.getNewValue() instanceof Tile) {
+					updateCityPanel((Tile) evt.getNewValue());
+				}
+			}
+		});
 		jpMap.setBackground(Color.BLACK);
-		JMapDesc descGame = new JMapDesc(jregion.getPersonnage());
+		JMapDesc descGame = new JMapDesc(pj);
 		jpMap.add(descGame);
 		jpMap.add(jregion);
 		Icon mapIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("map.png"));
 		this.addTab("Carte", mapIcon, jpMap, "Carte");
 		// Compétences
 		Icon compIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("comp.png"));
-		jpComp = new JPanSkill(jregion.getPersonnage().getSkills());
+		jpComp = new JPanSkill(pj.getSkills());
 		this.addTab("Compétences", compIcon, jpComp, "Compétences");
 		// Inventaire
 		Icon backpackIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("backpack.png"));
-		jpInv = new JPanInventory(jregion.getPersonnage());
+		jpInv = new JPanInventory(pj);
 		this.addTab("Inventaire", backpackIcon, jpInv, "Inventaire");
 		// Equipement
 		Icon equipIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("equip.png"));
-		jpEquip = new JTabEquip(jregion.getPersonnage());
+		jpEquip = new JTabEquip(pj);
 		this.addTab("Equipement", equipIcon, jpEquip, "Equipement");
 		// Magie
 		Icon magieIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("magie.png"));
 		this.addTab("Magie", magieIcon, jpMagie, "Magie");
 		// Quêtes
 		Icon questIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("quest.png"));
-		jpQuest = new JPanQuest(jregion.getPersonnage());
+		jpQuest = new JPanQuest(pj);
 		this.addTab("Quêtes", questIcon, jpQuest, "Quêtes");
 		// Villes
 		Icon cityIcon = new ImageIcon(JPanMain.class.getClassLoader().getResource("city.png"));
-		JPanCity jpCity = new JPanCity(jregion.getPersonnage());
+		JPanCity jpCity = new JPanCity(pj);
 		this.addTab("Ville", cityIcon, jpCity, "Ville");
-		this.setEnabledAt(CITY_PANEL_INDEX, jregion.getPersonnage().getTile().isCivilized());
+		updateCityPanel(pj.getTile());
 	}
 
-	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-		if (evt.getSource() instanceof Player) {
-			Player pj = (Player) evt.getSource();
-			this.setEnabledAt(CITY_PANEL_INDEX, pj.getTile().isCivilized());
-		}
+	private void updateCityPanel(Tile newTile) {
+		this.setEnabledAt(CITY_PANEL_INDEX, newTile.isCivilized());
 	}
 }
