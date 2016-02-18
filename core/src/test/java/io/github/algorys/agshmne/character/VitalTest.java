50932746174776fb3fb8b7619d4b1e81a2db64fe
Matthieu Estrada <ttamalfor@gmail.com>
2016-02-18 14:47:11 +0100

    Ajout Tests Unitaires pour Vital

--- core/src/test/java/io/github/algorys/agshmne/character/VitalTest.java
+++ core/src/test/java/io/github/algorys/agshmne/character/VitalTest.java
@@ -16,7 +16,7 @@ public class VitalTest {
 		underTest.setLife(underTest.getLife() - 4);
 		assertNotEquals(Vital.MAX_LIFE, underTest.getLife());
 	}
-	
+
 	@Test
 	public void vitalShouldHaveMaxManaEqualsInitialisation() {
 		Vital underTest = new Vital(15, 15, 10, 10);
@@ -31,11 +31,47 @@ public class VitalTest {
 		underTest.setLife(underTest.getLife() - 16);
 		assertEquals(0, underTest.getLife());
 	}
-	
+
 	@Test
 	public void manaCantDropBelowZero() {
 		Vital underTest = new Vital(15, 15, 10, 10);
 		underTest.setMana(underTest.getMana() - 16);
 		assertEquals(0, underTest.getMana());
+	}
+
+	@Test
+	public void hungerCantDropBelowZero() {
+		Vital underTest = new Vital(15, 15, 10, 10);
+		underTest.setHunger(underTest.getHunger() - 12);
+		assertEquals(0, underTest.getHunger());
+	}
+
+	@Test
+	public void fatigueCantDropBelowZero() {
+		Vital underTest = new Vital(15, 15, 10, 10);
+		underTest.setFatigue(underTest.getFatigue() - 12);
+		assertEquals(0, underTest.getFatigue());
+	}
+
+	@Test
+	public void whenHungerExceedsHisMax_LifeDecreases() {
+		Vital underTest = new Vital(15, 15, 10, 10);
+		int oldLife = underTest.getLife();
+
+		underTest.setHunger(12);
+		underTest.checkVital();
+
+		assertNotEquals(oldLife, underTest.getLife());
+	}
+
+	@Test
+	public void whenFatigueExceedsHisMax_LifeDecreases() {
+		Vital underTest = new Vital(15, 15, 10, 10);
+		int oldLife = underTest.getLife();
+
+		underTest.setFatigue(12);
+		underTest.checkVital();
+
+		assertNotEquals(oldLife, underTest.getLife());
 	}
 }
