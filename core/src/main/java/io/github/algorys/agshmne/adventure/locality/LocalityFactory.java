05496a8bcf92d943d2af25bddbd14cdfe6ea8a6a
Estrada Matthieu <ttamalfor@gmail.com>
2016-02-19 21:36:27 +0100

    Diversifie Locality

--- core/src/main/java/io/github/algorys/agshmne/adventure/locality/LocalityFactory.java
+++ core/src/main/java/io/github/algorys/agshmne/adventure/locality/LocalityFactory.java
@@ -15,17 +15,23 @@ public class LocalityFactory implements IAdventureFactory {
 	public LocalityFactory() {
 		locality = new ArrayList<>();
 		locality.add("Fontaine:LIFE10:"+
-				"Une fontaine représentant un ange déversant des flots sur un corps inanimé. Boire l'eau ?:Affecte votre vie !");
+				"Une fontaine représentant un ange déversant des flots sur un corps inanimé. Boire l'eau ?"
+				+ ":L'eau semble se propager dans tout votre corps. ");
 		locality.add("Temple:MANA5:"
-				+ "Vous apercevez un temple au loin. De grandes têtes de Tigres de Serleth surplombent l'entrée ! S'y reposer ?:Affecte votre Mana.");
+				+ "Vous apercevez un temple au loin. De grandes têtes de Tigres de Serleth surplombent l'entrée ! S'y reposer ?"
+				+ ":Vous rêvez que de longues formes blanches vous entourent. ");
 		locality.add("Portes:LIFE5:"
-				+ "D'anciennes portes semblent garder un souvenir d'une ancienne bataille... Passer les portes ?:Affecte votre vie !");
+				+ "D'anciennes portes semblent garder un souvenir d'une ancienne bataille... Voulez vous passer les portes ?"
+				+ ":Rien ne semble se passer... c'est votre intuition en tout cas. ");
 		locality.add("Verger:LIFE10:"
-				+ "Un verger luxurieux qui semblent vous acceuillir à bras ouvert. Manger les fruits du verger ?:Affecte votre vie !");
+				+ "Un verger luxurieux qui semblent vous acceuillir à bras ouvert. Manger les fruits du verger ?"
+				+ ":Ces fruits ne sont ni bons, ni mauvais. Mais ils sont tout mous. ");
 		locality.add("Grand Arbre:MANA10:"
-				+ "Cet arbre majestueux semble tellement ancien... et tellement grand ! Toucher l'arbre ?:L'énergie de l'arbre se déverse en vous. ");
-		locality.add("Stèle:CFOU1:"
-				+ "Une imposante stèle contenant des gravures anciennes se dresse devant vous. Pousser la stèle ?:Affecte votre compétence \"Fouiller\"");
+				+ "Cet arbre majestueux semble tellement ancien... et tellement grand ! Toucher l'arbre ?"
+				+ ":L'énergie de l'arbre se déverse en vous. ");
+		locality.add("Stèle:MANA5:"
+				+ "Une imposante stèle contenant des gravures anciennes se dresse devant vous. Pousser la stèle ?"
+				+ ":Les gravures deviennent rouges et s'illuminent. ");
 	}
 
 	
