b88614d8552fc5d2351b57db373431ddeb67940d
Matthieu Estrada <ttamalfor@gmail.com>
2016-02-23 14:44:34 +0100

    Modification .bat pour v0.3

--- packager/src/main/script/agshmne.bat
+++ packager/src/main/script/agshmne.bat
@@ -1,4 +1,4 @@
 rem mieux gérer les chemins (pour pouvoir appeler le script de n'importe où)
 
-SET CLASSPATH="lib/agshmne-swing-0.0.1-SNAPSHOT.jar;lib/agshmne-core-0.0.1-SNAPSHOT.jar"
+SET CLASSPATH="lib/agshmne-swing-0.0.3-ALPHA.jar;lib/agshmne-core-0.0.3-ALPHA.jar"
 java -cp "%CLASSPATH%"  io.github.algorys.agshmne.Agshmne
